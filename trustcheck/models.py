d5ae96a6036a4680bef9ccbe181c1fef4cabc66b
noble <noblebarch@gmail.com>
2024-08-15 15:32:56 -0400

    added category&datasubmission model to trustcheck

--- trustcheck/models.py
+++ trustcheck/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.SET_NULL, related_name='subcategories'
+    )
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name    
+
+class DataSubmission(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_verified = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Data Submission"
+        verbose_name_plural = "Data Submissions"
+
+    def __str__(self):
+        return self.title
